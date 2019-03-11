a5b8f8735ad82103174f58df1c7efc6c407f6a70
totolartichaud <43410154+totolartichaud@users.noreply.github.com>
2019-03-11 12:56:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,19 @@
-* {
-    box-sizing: border-box;
-}
+let navbarMenu = document.getElementById('navbarDrowndownMenu');
+let navDropper = document.getElementById('dropper');
+let menuOpen = false;
 
+navDropper.addEventListener('click', function() {
 
-/*FIRST NAV*/
+    if(menuOpen) {
 
-.navbar-1 {
-    position: relative;
-    background-color: rgba(0, 0, 0, 0.24);
-    height: 52px;
-    line-height: 52px;
-    text-align: center;
-}
+        menuOpen = false;
+        navbarMenu.style.display = "none";
 
-.navbar-1 a {
-    
-    text-decoration: none;
-    font-family: sans-serif;
-    color: #343434;
-    font-size: 18px;
-}
+    } else {
+        
+        menuOpen = true;
+        navbarMenu.style.display = "inline-block";
 
-.nav-logo {
-    float: left;
-    height: 100%;
-    margin-left: 20px;
-}
+    }
 
-.nav-logo a {
-    font-size: 22px;
-    font-weight: bold;
-    display: block;
-    padding: 0 10px;
-}
-
-.navbar-1 > ul {
-    transition: all .33s ease 0s;
-    font-size: 0;
-}
-
-.navbar-1 > ul > li:last-child {
-    margin-left: 10px;
-    position: relative;
-}
-
-.navbar-1 > ul, .navbar-1 > ul > li {
-    display: inline-block;
-    height: 100%;
-}
-
-.navbar-1 li a {
-    display: block;
-    padding: 0 15px;
-}
-
-.navbar-1 li a:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-}
-
-#navbarDrowndownMenu {
-    display: none;
-    position: absolute;
-    width: 200px;
-    left: 0;
-    top: 100%;
-    border: 1px solid rgba(128, 128, 128, 0.295);
-    border-radius: 4px;
-    -webkit-border-radius: 4px;
-    -moz-border-radius: 4px;
-    -ms-border-radius: 4px;
-    -o-border-radius: 4px;
-    margin-top: 15px;
-}
-
-#navbarDropdownMenu li {
-    width: 100%;
-}
-
-#navbarDropdownMenu li a {
-    height: 100%;
-    font-size: 15px ;
-}
+});
